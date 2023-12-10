cb23dc4ddb205edc1c3f2f417fa6a9e9b3893f30
d1msk1y <exect81@gmail.com>
2023-12-10 18:41:14 +0100

    Make theme params optional, and handle them

--- src/themes/gruvbox.rs
+++ src/themes/gruvbox.rs
@@ -3,10 +3,10 @@ use crate::theme_setter::ThemeParams;
 
 pub fn apply() {
     let params = ThemeParams {
-        kitty: String::from("/home/dmytro/.config/kitty/themes/Gruvbox Dark Soft.conf"),
-        wallpaper: String::from("/home/dmytro/documents/wallpapers/Gruvbox/minimalistic/ALLqk82.png"),
-        rofi: String::from("@theme \"/usr/share/rofi/themes/gruvbox-dark-soft.rasi\"" ),
-        polybar: String::from("gruvbox"),
+        kitty: Option::from(String::from("/home/dmytro/.config/kitty/themes/Gruvbox Dark Soft.conf")),
+        wallpaper: Option::from(String::from("/home/dmytro/documents/wallpapers/Gruvbox/minimalistic/ALLqk82.png")),
+        rofi: Option::from(String::from("@theme \"/usr/share/rofi/themes/gruvbox-dark-soft.rasi\"")),
+        polybar: Option::from(String::from("gruvbox")),
     };
 
     theme_setter::theme_setter::set_themes(params);
--- src/themes/gruvbox_light.rs
+++ src/themes/gruvbox_light.rs
@@ -3,10 +3,10 @@ use crate::theme_setter::ThemeParams;
 
 pub fn apply() {
     let params = ThemeParams {
-        kitty: String::from("/home/dmytro/.config/kitty/themes/Gruvbox Material Light Medium.conf"),
-        wallpaper: String::from("/home/dmytro/documents/wallpapers/Gruvbox/light/cyber-girl-light.png"),
-        rofi: String::from("@theme \"/usr/share/rofi/themes/gruvbox-light-hard.rasi\""),
-        polybar: String::from("gruvbox-light"),
+        kitty: Option::from(String::from("/home/dmytro/.config/kitty/themes/Gruvbox Material Light Medium.conf")),
+        wallpaper: Option::from(String::from("/home/dmytro/documents/wallpapers/Gruvbox/light/cyber-girl-light.png")),
+        rofi: Option::from(String::from("@theme \"/usr/share/rofi/themes/gruvbox-light-hard.rasi\"")),
+        polybar: Option::from(String::from("gruvbox-light")),
     };
 
     theme_setter::theme_setter::set_themes(params);
