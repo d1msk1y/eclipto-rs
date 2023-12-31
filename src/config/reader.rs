90f84c7c4fd9e2b0ae47f5179f7a77d97d4c0f8a
d1msk1y <exect81@gmail.com>
2023-12-31 22:57:06 +0100

    Read themes from dedicated config dir, if empty - create

--- src/config/reader.rs
+++ src/config/reader.rs
@@ -1,26 +1,21 @@
+use std::env;
+use std::path::Path;
+
 use crate::theme::ThemeList;
 
 pub fn parse_themes() -> Result<ThemeList, serde_json::Error> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-        {
-    "themes": {
-        "gruvbox": {
-            "kitty": "/path/to/kitty/theme",
-            "wallpaper": "/path/to/wallpaper",
-            "rofi": "/path/to/rofi/theme",
-            "polybar": "theme_name"
-        },
-        "gruvbox-light": {
-            "kitty": "/path/to/kitty/theme",
-            "wallpaper": "/path/to/wallpaper",
-            "rofi": "/path/to/rofi/theme",
-            "polybar": "theme_name"
-        }
+    const CONFIG_DIR: &str = &*(env::var("HOME").unwrap() + "/.config/eclipto/");
+
+    if !Path::new(&(CONFIG_DIR.to_string() + "themes.json")).exists() {
+        println!("No themes found, creating default themes.json");
+        std::fs::write(CONFIG_DIR.to_string() + "themes.json", "define your themes here").expect("Unable to write file");
+        // quit the program
+        std::process::exit(0);
     }
-}
-"#;
-    let result: ThemeList = serde_json::from_str(data)?;
+    let contents = std::fs::read_to_string(CONFIG_DIR.to_string() + "themes.json")
+        .expect("Something went wrong reading the file");
+
+    let result: ThemeList = serde_json::from_str(&*contents)?;
 
     Ok(result)
 }
